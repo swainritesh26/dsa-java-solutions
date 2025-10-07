bb14657f29273f6b48fd7b2bbe751eaf0d423214
swainritesh26 <riteshswain37@gmail.com>
2025-10-07 12:18:02 +0530

    Added split array with the largest sum

--- src/binarysearch/SplitSubArrayLargestSum.java
+++ src/binarysearch/SplitSubArrayLargestSum.java
@@ -1,13 +1,40 @@
-//package binarysearch;
-//
-//public class SplitSubArrayLargestSum {
-//
-//    public static int splitArray(int[] arr, int k) {
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        int[] arr = {7,2,5,10,8};
-//        int k = 2;
-//    }
-//}
+package binarysearch;
+
+public class SplitSubArrayLargestSum {
+
+    public static int splitArray(int[] arr, int k) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int num : arr){
+            sum += num;
+            max = Math.max(max,num);
+        }
+        for(int i=max;i<=sum;i++){
+            if(countSubArray(arr,i) == k){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int countSubArray(int[] arr,int sum){
+        int subArray = 1;
+        int sumArray = 0;
+        for(int num : arr){
+            if(sumArray + num <= sum){
+                sumArray += num;
+            }
+            else{
+                subArray++;
+                sumArray = num;
+            }
+        }
+        return subArray;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7,2,5,10,8};
+        int k = 2;
+        System.out.println(splitArray(arr,k));
+    }
+}
